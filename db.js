f9d44e67d4f562f633e298e5435a0b64a3a97eb5
zhangdezheng <2549626703@qq.com>
2024-12-21 12:42:04 +0800

    init database config

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 // 从环境变量中读取数据库配置
 const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+console.log('zdz', process.env)
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
